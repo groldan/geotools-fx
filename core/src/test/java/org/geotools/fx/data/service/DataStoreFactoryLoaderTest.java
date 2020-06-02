55393bb93eaaed694108ac86fde67f096e1ddc03
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-06-02 00:24:22 -0300

    Format pom and license headers

--- core/src/test/java/org/geotools/fx/data/service/DataStoreFactoryLoaderTest.java
+++ core/src/test/java/org/geotools/fx/data/service/DataStoreFactoryLoaderTest.java
@@ -1,15 +1,18 @@
 /*
- * GeoTools - The Open Source Java GIS Toolkit http://geotools.org
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
  *
- * (C) 2019, Open Source Geospatial Foundation (OSGeo) (C)
+ *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
  *
- * This library is free software; you can redistribute it and/or modify it under the terms of the
- * GNU Lesser General Public License as published by the Free Software Foundation; version 2.1 of
- * the License.
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 package org.geotools.fx.data.service;
 
@@ -38,7 +41,6 @@ import org.geotools.data.DataStoreFactorySpi;
 import org.geotools.data.property.PropertyDataStoreFactory;
 import org.geotools.data.shapefile.ShapefileDataStoreFactory;
 import org.geotools.fx.data.model.DataStoreFactory;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -53,11 +55,11 @@ public class DataStoreFactoryLoaderTest {
     private TestDataStoreFactory testDataStoreFactory;
 
     public static @BeforeClass void beforeClass() {
-        Platform.startup(() -> {});
-    }
+        try {
+            Platform.startup(() -> {});
+        } catch (IllegalStateException toolkitAlreadyInitialized) {
 
-    public static @AfterClass void afterClass() {
-        Platform.exit();
+        }
     }
 
     public @Before void before() {
